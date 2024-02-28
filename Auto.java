00c04dae47171b623515cb4d2d0acdb2987f1f46
camilofernandezuno@gmail.com <camilofernandezuno@gmail.com>
2024-02-27 20:12:02 -0500

    Finish

--- Auto.java
+++ Auto.java
@@ -15,37 +15,48 @@ public class Auto {
 	Scanner scanner = new Scanner(System.in);
 
 	public void set_ano_serie() {
-
 		System.out.println("Por favor ingresa el año de serie del vehiculo:");
 		ano_serie = scanner.nextInt();
 		scanner.nextLine();
 	}
 
 	public void set_marca_auto() {
-
 		System.out.println("Por favor ingresa la marca del auto:");
-		String marca_auto = scanner.nextLine();
-
+		marca_auto = scanner.nextLine();
 	}
 
 	public void set_color() {
-
 		System.out.println("Por favor ingresa el color del auto:");
-		String color = scanner.nextLine();
-
+		color = scanner.nextLine();
 	}
 
 	public void set_total_autos_s() {
-
 		System.out.println("Por favor ingresa el total de autos por serie:");
 		total_autos_s = scanner.nextInt();
-
 	}
 
 	public void set_numero_serie() {
-
 		System.out.println("Por favor ingresa el numero dentro de la serie:");
 		numero_serie = scanner.nextInt();
+	}
 
+	public int get_numero_serie() {
+		return this.numero_serie;
+	}
+
+	public int get_total_autos_s() {
+		return this.total_autos_s;
+	}
+
+	public int get_ano_serie() {
+		return this.ano_serie;
+	}
+
+	public String get_marca_auto() {
+		return this.marca_auto;
+	}
+
+	public String get_color() {
+		return this.color;
 	}
 }
--- Lista.java
+++ Lista.java
@@ -8,16 +8,17 @@ public class Lista {
 	private List<Auto> collecionautos = new ArrayList<>();
 
 	public Lista() {
-
 	}
 
-	int respuesta;
 	Scanner scanner = new Scanner(System.in);
 
 	public void llenar() {
 		while (true) {
-			System.out.println("Desea continuar con el programa\n Digite 1 para si \n Digite 2 para no");
-			respuesta = scanner.nextInt();
+			System.out.println("¿Que desea hacer con el programa?" + "\n Digite 1 | Añadir un auto.  "
+					+ "\n Digite 2 | Eliminar auto de la lista. "
+					+ "\n Digite 3 | Verificar auto dentro de la collecion." + "\n Digite 4 | Terminar programa.");
+
+			int respuesta = scanner.nextInt();
 
 			if (respuesta == 1) {
 				Auto inidividual_car = new Auto();
@@ -32,11 +33,51 @@ public class Lista {
 				inidividual_car.set_numero_serie();
 
 				collecionautos.add(inidividual_car);
+				System.out.println("Auto agregado correctamente.");
+			} else if (respuesta == 2) {
+				this.eliminar();
+			} else if (respuesta == 3) {
+				this.verificar();
 			} else {
 				break;
-
 			}
 		}
-		scanner.close();
 	}
+
+	public void eliminar() {
+		System.out.println("Estos son los carros de la lista");
+		for (Auto auto : collecionautos) {
+			System.out.println("Año de serie: " + auto.get_ano_serie());
+			System.out.println("Marca: " + auto.get_marca_auto());
+			System.out.println("Color: " + auto.get_color());
+			System.out.println("Total de autos por serie: " + auto.get_total_autos_s());
+			System.out.println("Número de serie: " + auto.get_numero_serie());
+			System.out.println("-----------------------------");
+			System.out.println("¿Desea Eliminar este auto ?" + "\n Digite 1 | Para SI.  " + "\n Digite 2 | Para NO.");
+			int respuesta_eliminar = scanner.nextInt();
+			if (respuesta_eliminar == 1) {
+				collecionautos.remove(auto);
+			}
+		}
+	}
+
+	public void verificar() {
+		System.out.println("Digite el año de serie del auto");
+		int respuesta_verificar1 = scanner.nextInt();
+
+		System.out.println("Digite el numero dentro de la serie del auto");
+		int respuesta_verificar2 = scanner.nextInt();
+
+		for (Auto auto : collecionautos) {
+			if (auto.get_ano_serie() == respuesta_verificar1 && auto.get_numero_serie() == respuesta_verificar2) {
+				System.out.println("El auto si esta dentro de la collecion");
+				break;
+			} else {
+				System.out.println("El auto no esta dentro de la collecion");
+
+			}
+
+		}
+	}
+
 }
